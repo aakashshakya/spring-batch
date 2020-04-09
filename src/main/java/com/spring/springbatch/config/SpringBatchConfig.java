90a6b99375adb66e936ccca7ed2380afe66db8c1
aakash <aakash_shakya@hotmail.com>
2020-04-09 11:50:27 +0545

    Spring batch initial setup for demo project.

---
+++ src/main/java/com/spring/springbatch/config/SpringBatchConfig.java
@@ -0,0 +1,73 @@
+package com.spring.springbatch.config;
+
+import com.spring.springbatch.model.User;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+
+@Configuration
+@EnableBatchProcessing
+public class SpringBatchConfig {
+    @Bean
+    public Job job(JobBuilderFactory jobBuilderFactory,
+                   StepBuilderFactory stepBuilderFactory,
+                   ItemReader<User> reader,
+                   ItemProcessor<User, User> process,
+                   ItemWriter<User> writer
+    ) {
+
+        Step step = stepBuilderFactory.get("user-insertion")
+                .<User, User>chunk(100000)
+                .reader(reader)
+                .processor(process)
+                .writer(writer)
+                .build();
+
+        return jobBuilderFactory.get("user-insertion")
+                .incrementer(new RunIdIncrementer())
+                .start(step)
+                .build();
+    }
+
+    @Bean
+    public FlatFileItemReader<User> itemReader() {
+        FlatFileItemReader<User> flatFileItemReader = new FlatFileItemReader<>();
+        flatFileItemReader.setResource(new FileSystemResource("src/main/resources/users.csv"));
+        flatFileItemReader.setName("CSV-Reader");
+        flatFileItemReader.setLinesToSkip(1);
+        flatFileItemReader.setLineMapper(getLineMapper());
+        return flatFileItemReader;
+    }
+
+    @Bean
+    public LineMapper<User> getLineMapper() {
+        DefaultLineMapper<User> defaultLineMapper = new DefaultLineMapper<>();
+
+        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+        lineTokenizer.setDelimiter(",");
+        lineTokenizer.setStrict(false);
+        lineTokenizer.setNames("name", "department", "salary");
+
+        BeanWrapperFieldSetMapper<User> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
+        fieldSetMapper.setTargetType(User.class);
+
+        defaultLineMapper.setLineTokenizer(lineTokenizer);
+        defaultLineMapper.setFieldSetMapper(fieldSetMapper);
+
+        return defaultLineMapper;
+    }
+}
---
+++ src/main/java/com/spring/springbatch/model/User.java
@@ -0,0 +1,23 @@
+package com.spring.springbatch.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String department;
+    private Integer salary;
+}
