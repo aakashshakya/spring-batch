cf6982fdeeb54d904f035e9e8547c0b85e6f9c78
aakash <aakash_shakya@hotmail.com>
2020-04-16 14:49:35 +0545

    User is now able to add department from the url. The department sent is checked in db and set in user accordingly.

--- src/main/java/com/spring/springbatch/batch/DBWriter.java
+++ src/main/java/com/spring/springbatch/batch/DBWriter.java
@@ -5,6 +5,10 @@ import com.spring.springbatch.model.User;
 import com.spring.springbatch.repository.DepartmentRepository;
 import com.spring.springbatch.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.annotation.BeforeStep;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -17,7 +21,7 @@ public class DBWriter implements ItemWriter<User> {
     private final UserRepository userRepository;
     private final DepartmentRepository departmentRepository;
     private static boolean isSetDepartment;
-    private static Department department;
+    private JobExecution jobExecution;
 
     @Autowired
     public DBWriter(UserRepository userRepository, DepartmentRepository departmentRepository) {
@@ -26,15 +30,34 @@ public class DBWriter implements ItemWriter<User> {
     }
 
     @Override
-    public void write(List<? extends User> users) throws Exception {
-        if(!isSetDepartment){
-            department = departmentRepository.save(new Department("Development"));
+    public void write(List<? extends User> users) {
+        JobParameters parameters = jobExecution.getJobParameters();
+        String departmentName =  parameters.getString("department");
+        Department department = departmentRepository.findByName(departmentName);
+        if(department != null){
+            Department finalDepartment = department;
+            users.forEach(user -> user.setDepartment(finalDepartment));
             isSetDepartment = true;
+        } else {
+            if(!isSetDepartment){
+                department = departmentRepository.save(new Department(departmentName));
+                Department finalDepartment1 = department;
+                users.forEach(user -> user.setDepartment(finalDepartment1));
+                isSetDepartment = true;
+            } else {
+                department = departmentRepository.save(new Department(departmentName));
+                Department finalDepartment1 = department;
+                users.forEach(user -> user.setDepartment(finalDepartment1));
+            }
         }
-        users.forEach(user -> user.setDepartment(department));
 
         log.info("Inserting value of user with data size: {}", users.size());
         userRepository.saveAll(users);
+    }
+
+    @BeforeStep
+    public void beforeStep(StepExecution stepExecution) {
+        jobExecution = stepExecution.getJobExecution();
     }
 }
 
