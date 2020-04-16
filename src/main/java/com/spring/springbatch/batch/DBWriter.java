79b5dee4be1ce6afd827d2cf96ada78da06a2e1f
aakash <aakash_shakya@hotmail.com>
2020-04-16 09:23:21 +0545

    Saving department as a new entity for user

--- src/main/java/com/spring/springbatch/batch/DBWriter.java
+++ src/main/java/com/spring/springbatch/batch/DBWriter.java
@@ -1,6 +1,8 @@
 package com.spring.springbatch.batch;
 
+import com.spring.springbatch.model.Department;
 import com.spring.springbatch.model.User;
+import com.spring.springbatch.repository.DepartmentRepository;
 import com.spring.springbatch.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.ItemWriter;
@@ -13,14 +15,24 @@ import java.util.List;
 @Slf4j
 public class DBWriter implements ItemWriter<User> {
     private final UserRepository userRepository;
+    private final DepartmentRepository departmentRepository;
+    private static boolean isSetDepartment;
+    private static Department department;
 
     @Autowired
-    public DBWriter(UserRepository userRepository) {
+    public DBWriter(UserRepository userRepository, DepartmentRepository departmentRepository) {
         this.userRepository = userRepository;
+        this.departmentRepository = departmentRepository;
     }
 
     @Override
     public void write(List<? extends User> users) throws Exception {
+        if(!isSetDepartment){
+            department = departmentRepository.save(new Department("Development"));
+            isSetDepartment = true;
+        }
+        users.forEach(user -> user.setDepartment(department));
+
         log.info("Inserting value of user with data size: {}", users.size());
         userRepository.saveAll(users);
     }
